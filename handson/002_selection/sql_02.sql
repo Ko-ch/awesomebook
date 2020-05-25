90e91813c9e1e53602d1d199527c33b038a513af
Koichiro <koichiro_takagi@outlook.jp>
2020-05-25 22:27:10 +0900

    1 commit

---
+++ handson/002_selection/sql_02.sql
@@ -0,0 +1,17 @@
+-- a_sql_1_not_awesome.sql
+select
+    *
+from
+    awesomebook.reserve
+where
+    checkin_date between '2016-10-12' and '2016-10-13'
+
+
+-- b_sql_2_awesome.sql
+select
+    *
+from
+    awesomebook.reserve
+where
+    checkin_date  between '2016-10-10' and '2016-10-13'
+    checkout_date between '2016-10-13' and '2016-10-14'
